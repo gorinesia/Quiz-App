bf291a689a92d30f17a8f24bc99be3750ef865e4
Akinori Sakakibara <gorion.1213@gmail.com>
2020-07-31 16:37:31 +0900

    initial commit for feature branch

--- main.js
+++ main.js
@@ -4,102 +4,125 @@
   // 利用するAPI
   const API_URL = 'https://opentdb.com/api.php?amount=10&type=multiple';
 
-  // クイズアプリのデータ管理用オブジェクトを用意する。
+  // クイズアプリのデータ管理用オブジェクト
   const gameState = {
     quizzes: [],
     currentIndex: 0,
     numberOfCorrects: 0
   };
 
-  // HTMLのid値がセットされているDOMを取得する。
+
+  class Quiz {
+    constructor(quizData) {
+      this.quizzes = quizData.results;
+    }
+
+    getQuizCategory() {
+      return this.quizzes.category;
+    }
+
+    getQuizDifficulty() {
+      return this.quizzes.difficulty;
+    }
+
+    getNumOfQuiz() {
+      return this.quizzes.length;
+    }
+
+    getQuizQuestion() {
+      return this.quizzes.question;
+    }
+
+    getCorrectAnswer() {
+      return this.quizzes.correct_answer;
+    }
+
+    getIncorrectAnswers() {
+      return this.quizzes.incorrect_answers;
+    }
+  }
+  
+  // HTMLのid値がセットされているDOMを取得する
   const titleElement = document.getElementById('title');
   const questionElement = document.getElementById('question');
   const answersContainer = document.getElementById('answers');
   const startButton = document.getElementById('start-button');
-  const genreElement = document.getElementById('genre');
+  const genreElement = document.getElementById('gerne');
   const difficultyElement = document.getElementById('difficulty');
 
-  /**
-  * 「Quiz」クラスを作成する
-  * クイズデータを元にWebページ上に問題と解答リストを表示する。
-  * 解答をクリックし、正解であれば正答数をインクリメントする。
-  * 回答する度に問題数もインクリメントする。
-  * setNwxtQuiz関数を実行して次の問題をセットする（最後の問題の場合は結果を表示する）。
-  */
- class Quiz {
-   constructor(quiz) {
-     this.quiz = quiz;
-   }
 
-   makeQuiz(quiz) {
-     const answers = buildAnswers(quiz);
-     
-     titleElement.textContent = `問題 ${gameState.currentIndex + 1}`;
-     genreElement.textContent = `【ジャンル】 ${quiz.category}`;
-     difficultyElement.textContent = `【難易度】 ${quiz.difficulty}`;
-     questionElement.textContent = unescapeHTML(quiz.question);
-     
-     answers.forEach((answer) => {
-       const answerElement = document.createElement('li');
-       answersContainer.appendChild(answerElement);
-       
-       const buttonElement = document.createElement('button');
-       buttonElement.textContent = unescapeHTML(answer);
-       answerElement.appendChild(buttonElement);
-       
-       answerElement.addEventListener('click', () => {
-         const correctAnswer = unescapeHTML(quiz.correct_answer);
-         if (correctAnswer === answerElement.textContent) {
-           gameState.numberOfCorrects++;
-          }
-          
-          gameState.currentIndex++;
-          setNextQuiz();
-        });
-      });
-    };
-  }
-
-  // 「Quiz」クラスのインスタンスを生成
-  const quizInstance = new Quiz();
-
-  // 「開始」ボタンをクリックしたらクイズ情報を取得する。
+  // 「開始」ボタンをクリックしたらクイズ情報を取得する
   startButton.addEventListener('click', () => {
     startButton.hidden = true;
     fetchQuizData();
   });
 
-  // Webページ上の表示をリセットする。
-  // fetch APIを使い、API経由でデータを取得する。
+  // Webページ上の表示をリセットする
+  // fetch APIを使い、API経由でデータを取得する
   const fetchQuizData = async () => {
     titleElement.textContent = '取得中';
     questionElement.textContent = '少々お待ち下さい';
 
     const response = await fetch(API_URL);
-    const data = await response.json();
-
-    gameState.quizzes = data.results;
+    const quizData = await response.json();
+    const quizInstance = new Quiz(quizData);
+    
+    gameState.quizzes = quizData.results;
     gameState.currentIndex = 0;
     gameState.numberOfCorrects = 0;
     
-    setNextQuiz();
+    setNextQuiz(quizInstance);
   };
 
-  // 表示要素をリセットする。
-  // 条件に応じて、次の問題の表示 or 結果を表示する。
-  const setNextQuiz = () => {
+  /*
+   クイズデータを元にWebページ上に問題と解答リストを表示する
+   解答をクリックし、正解であれば正答数をインクリメントする
+   回答する度に問題数プロパティもインクリメントする
+   setNwxtQuiz関数を実行して次の問題をセットする（最後の問題の場合は結果を表示する）。
+   */
+  const makeQuiz = (quizInstance) => {
+
+    const answers = buildAnswers(quizInstance);
+    
+    titleElement.innerHTML = `問題 ${quizInstance.currentIndex + 1}`;
+    genreElement.innerHTML = `【ジャンル】 ${quizInstance.getQuizCategory()}`;
+    difficultyElement.innerHTML = `【難易度】 ${quizInstance.getQuizDifficulty()}`;
+    questionElement.innerHTML = unescapeHTML(quizInstance.getQuizQuestion());
+
+    answers.forEach((answer) => {
+      const answerElement = document.createElement('li');
+      answersContainer.appendChild(answerElement);
+
+      const buttonElement = document.createElement('button');
+      buttonElement.innerHTML = unescapeHTML(answer);
+      answerElement.appendChild(buttonElement);
+
+      answerElement.addEventListener('click', () => {
+        const correctAnswer = unescapeHTML(quizInstance.correct_answer);
+        if (correctAnswer === answerElement.innerHTML) {
+          gameState.numberOfCorrects++;
+        }
+
+        gameState.currentIndex++;
+        setNextQuiz(quizInstance);
+      });
+    });
+  }
+
+  // 表示要素をリセットする
+  // 条件に応じて、次の問題の表示 or 結果を表示する
+  const setNextQuiz = (quizInstance) => {
     removeAllAnswers();
 
     if (gameState.currentIndex < gameState.quizzes.length) {
-      const quiz = gameState.quizzes[gameState.currentIndex];
-      quizInstance.makeQuiz(quiz);
+      makeQuiz(quizInstance);
     } else {
       finishQuiz();
     }
   };
 
-  // クイズを解いた結果を表示する。
-  // 「ホームに戻る」ボタンを表示する。
+  // クイズを解いた結果を表示する
+  // 「ホームに戻る」ボタンを表示する
   const finishQuiz = () => {
     titleElement.textContent = `あなたの正答数は${gameState.numberOfCorrects}です`
     genreElement.textContent = '';
@@ -114,27 +137,25 @@
     });
   };
 
-  // 回答を全て削除する。
+  // 回答を全て削除する
   const removeAllAnswers = () => {
     while (answersContainer.firstChild) {
       answersContainer.removeChild(answersContainer.firstChild);
     }
   };
-
-  /*
-   quizオブジェクトの中にあるcorrect_answer, incorrect_answersを結合して、
-   正解・不正解の解答をシャッフルする。
-  */
-  const buildAnswers = (quiz) => {
+  
+  // quizオブジェクトの中にあるcorrect_answer, incorrect_answersを結合して
+  // 正解・不正解の解答をシャッフルする
+  const buildAnswers = (quizInstance) => {
     const answers = [
-      quiz.correct_answer,
-      ...quiz.incorrect_answers
+      quizInstance.getCorrectAnswer(),
+      ...quizInstance.getIncorrectAnswers()
     ];
     return shuffle(answers);
   };
 
-  // 引数で受け取った配列内の値をシャッフルする。
-  // 引数で渡された配列を上書きしないように、シャッフルする配列はコピーしたものを使う。
+  // 引数で受け取った配列内の値をシャッフルする
+  // 引数で渡された配列を上書きしないように、シャッフルする配列はコピーしたものを使う
   const shuffle = (array) => {
     const copiedArray = array.slice();
 
@@ -145,10 +166,8 @@
     return copiedArray;
   };
 
-  /*
-   &やクォーテーションマークなどが特殊文字としてセットされているので、
-   人間が読みやすい形式に変換した文字列を取得する。
-  */
+  // &やクォーテーションマークなどが特殊文字としてセットされているので、
+  // 人間が読みやすい形式に変換した文字列を取得する
   const unescapeHTML = (str) => {
     const div = document.createElement("div");
     div.innerHTML = str.replace(/</g, "&lt;")
